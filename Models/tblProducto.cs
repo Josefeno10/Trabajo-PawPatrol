d02ee489ca1cd259ae4eb2b867c3867e27c7d3d6
allisga <2230268@upt.edu.mx>
2025-06-09 22:41:03 -0600

    Se arregló el problema de la validación de datos en el modelo de la tabla productos

--- Models/tblProducto.cs
+++ Models/tblProducto.cs
@@ -11,7 +11,8 @@ namespace PedidosComida.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class tblProducto
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
